93551c442cf8395d40c6c49b298cdd3ab0ba54fe
pedro josé lima <pedrlimadev@gmail.com>
2023-10-22 10:07:27 -0300

    fix: remove test for deploy

--- Tests/passwordPattern.spec.ts
+++
@@ -1,8 +0,0 @@
-import { describe, expect, test } from "@jest/globals";
-import { passwordPattern } from "../src/Middlewares/PasswordMiddleware";
-
-describe("password pattern module", () => {
-  test("valid password pattern", () => {
-    expect(passwordPattern("123456781#")).toBe(true);
-  });
-});
